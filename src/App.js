fae1817d556f5fcb7bde40796c948f0292df7f19
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-08-29 12:51:43 +0100

    feat: getting users from an test API and adding loading for possible time problems

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
@@ -18,18 +18,28 @@ class App extends Component {
         this.setState( { searchfield: event.target.value } );
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response =>  response.json()) // passing the data to json - because it receaves a promise
+            .then(users => this.setState({ robots: users })); // and setting the robots to the users 
+    }
+
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RobotFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RobotFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
